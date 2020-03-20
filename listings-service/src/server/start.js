21fe847505fb592ecadfd6ea1bd74c5a778fb0a5
Dany Dodson <danydodson@gmail.com>
2020-03-19 22:51:49 -0500

    updated

--- listings-service/src/server/start.js
+++ listings-service/src/server/start.js
@@ -2,11 +2,11 @@ const express = require('express')
 
 const app = express()
 
-// const PORT = accessEnv('PORT', 7000)
-const PORT = 7000
+// const PORT = accessEnv('PORT', 7100)
+const PORT = 7100
 
 const listener = app.listen(PORT, "0.0.0.0", () => {
-  console.info(`api-gateway listening on port ${listener.address().port}`)
+  console.info(`listings-service listening on port ${listener.address().port}`)
 })
 
 module.exports = listener

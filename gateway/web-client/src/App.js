163ad19bcdf265df2e705b56f7b33e148ddb43e1
Dany Dodson <danydodson@gmail.com>
2020-04-15 20:13:44 -0500

    gateway web client

--- gateway/web-client/src/App.js
+++ gateway/web-client/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Web-Client !!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    welcome: "..."
+  };
+
+  componentDidMount = async () => {
+    try {
+      const res = await axios.get("/welcome");
+      console.log(res);
+      this.setState({ welcome: res.data });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>"Hello server!" says the client</h1>
+        <h1>"{this.state.welcome}" says the server</h1>
+      </div>
+    );
+  }
 }
 
 export default App;

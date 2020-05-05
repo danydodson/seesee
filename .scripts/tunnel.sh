a851937420c129f44a5e5509643e80d11984db68
Dany Dodson <danydodson@gmail.com>
2020-05-05 06:23:49 -0500

    updates

--- .scripts/tunnel.sh
+++ .scripts/tunnel.sh
@@ -4,6 +4,13 @@ valid=true
 
 if [ $valid ]
 then
-    echo "[TUNNEL] running danydodson/seesee_dev_proxy" && \
-    docker run -d -P 80:80 -p 2222:22 danydodson/seesee_dev_proxy
+    # echo "[TUNNEL] running danydodson/seesee_dev_proxy" && \
+    # docker build -t danydodson/proxy-tunnel . --build-arg ROOTPW=password && \
+    # docker run -d -P 80:80 -p 2222:22 danydodson/seesee_dev_proxy
 fi
+
+    # ssh -NR :80:localhost:3000 -p 2222 root@YOURDOMAIN.tld
+
+# build with password
+
+# run from local computer

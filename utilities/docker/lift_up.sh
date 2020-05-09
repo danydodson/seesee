58fa4837b43569f24611aad4750d676be52552cb
Dany Dodson <danydodson@gmail.com>
2020-05-08 22:15:55 -0500

    update

--- utilities/docker/lift_up.sh
+++ utilities/docker/lift_up.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/zsh
 
-lift=true
+set -e
+
+# up=true
 
 if [ ! -z "(docker networks ls -f name=services_network)" ]
 then
@@ -10,15 +12,17 @@ else
     echo "[BUILD] services_network exists"
 fi
 
-set -e
-
-if [ $lift ]
+if [ $up ]
 then
     echo "[LIFTING] creating containers"
     docker-compose -f ../../docker-compose.dev.yml build
-    docker-compose -f ../../docker-compose.dev.yml up -d db
+    docker-compose -f ../../docker-compose.dev.yml $up -d auth_data
+    docker-compose -f ../../docker-compose.dev.yml $up -d users_data
+    docker-compose -f ../../docker-compose.dev.yml $up -d gallery_data
     sleep 5
-    docker-compose -f ../../docker-compose.dev.yml stop db
+    docker-compose -f ../../docker-compose.dev.yml stop auth_data
+    docker-compose -f ../../docker-compose.dev.yml stop users_data
+    docker-compose -f ../../docker-compose.dev.yml stop gallery_data
 fi
 
 

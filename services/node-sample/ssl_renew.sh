ba35fc4ab66aba09de3604ea1167b847116bc55b
Dany Dodson <danydodson@gmail.com>
2020-05-21 22:38:31 -0500

    changes

--- services/node-sample/ssl_renew.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-COMPOSE="/usr/local/bin/docker-compose --no-ansi"
-DOCKER="/usr/bin/docker"
-
-cd /home/ubuntu/node_project/
-# $COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
-$COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
-$DOCKER system prune -af

b5975ce12c910b56f76fb2ee412b8cd4580cb999
Dany Dodson <danydodson@gmail.com>
2020-05-21 19:05:53 -0500

    moved tests

--- utilities/docker/build.sh
+++ utilities/docker/build.sh
@@ -11,21 +11,3 @@ fi
 echo "[BUILD] building and lifting..."
 docker-compose -f ../../docker-compose.yml build --no-cache
 docker-compose -f ../../docker-compose.yml up -d
-
-# echo "[BUILD] Building users service db..."
-# docker-compose -f ../../docker-compose.yml up users_service_db
-
-# echo "[BUILD] Building users service..."
-# docker-compose -f ../../docker-compose.yml up users_service
-
-# docker-compose -f ../../docker-compose.dev.yml stop users_data
-# docker-compose -f ../../docker-compose.dev.yml stop users_services
-
-# echo "[BUILD] building danydodson/seesee_proxy_tunnel..."
-# docker build -t danydodson/seesee_proxy_tunnel "$dir/utilities/tunnel" --build-arg ROOTPW=password
-
-# echo "[BUILD] building danydodson/seesee_gateway_client..."
-# docker build --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') -t danydodson/seesee_gateway_client "$working-dir/gateway/client"
-
-# echo "[BUILD] building danydodson/seesee_..."
-# docker build -t danydodson/seesee_ "$dir"

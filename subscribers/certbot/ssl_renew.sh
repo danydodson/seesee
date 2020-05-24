85669c1d0f711c8973ba4afd3e7df963a673612d
Dany Dodson <danydodson@gmail.com>
2020-05-23 23:43:13 -0500

    changes

---
+++ subscribers/certbot/ssl_renew.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+COMPOSE="/usr/local/bin/docker-compose --no-ansi"
+DOCKER="/usr/bin/docker"
+
+cd /home/ubuntu/seesee/
+$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP nginx_proxy
+# $COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP nginx_proxy
+$DOCKER system prune -af

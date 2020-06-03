84b519fbcadfb2c31b8c1a06c193ea1c6fa92072
Dany Dodson <danydodson@gmail.com>
2020-06-03 13:21:05 -0500

    changes

--- services/listings/sequelize/config.js
+++ services/listings/sequelize/config.js
@@ -3,5 +3,5 @@ require('dotenv').config()
 module.exports.development = {
   dialect: 'mysql',
   seederStorage: 'sequelize',
-  url: process.env.DB_URI || 'mysql://localhost@listings_service_db/db?charset=UTF8',
+  url: process.env.DB_URI,
 }
--- services/users/sequelize/config.js
+++ services/users/sequelize/config.js
@@ -3,5 +3,6 @@ require('dotenv').config()
 module.exports.development = {
   dialect: 'mysql',
   seederStorage: 'sequelize',
-  url: process.env.DB_URI || 'mysql://localhost@users_service_db/db?charset=UTF8',
+  url: process.env.USERS_SERVICE_DB_URI,
+  // url: process.env.DB_URI,
 }

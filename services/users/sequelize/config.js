5ee9e475cc5f754573a5be1e3461398a6bf24237
Dany Dodson <danydodson@gmail.com>
2020-06-04 00:42:54 -0500

    changes

--- services/users/sequelize/config.js
+++ services/users/sequelize/config.js
@@ -3,6 +3,5 @@ require('dotenv').config()
 module.exports.development = {
   dialect: 'mysql',
   seederStorage: 'sequelize',
-  url: process.env.USERS_SERVICE_DB_URI,
-  // url: process.env.DB_URI,
+  url: process.env.DB_URI,
 }
--- utilities/compose/build.sh
+++ utilities/compose/build.sh
@@ -1,11 +1,11 @@
 
 #!/usr/bin/bash
 
-if [ ! -z "(docker networks ls -f name=services_network)" ]
-then
-    echo "[BUILD] Building docker services_network..."
-    docker network create services_network
-fi
+# if [ ! -z "(docker networks ls -f name=services_network)" ]
+# then
+    # echo "[BUILD] Building docker services_network..."
+    # docker network create services_network
+# fi
 
 echo "[BUILD] building and lifting..."
 docker-compose -f ../../docker-compose.yml build --no-cache

cb48875df6d0be87d5146e6cf72f377d33858deb
Dany Dodson <danydodson@gmail.com>
2020-06-04 02:25:31 -0500

    changes

--- gateway/passport/jest.config.js
+++ gateway/passport/jest.config.js
@@ -1,22 +1,29 @@
 module.exports = {
-    globals: {
-        "ts-jest": {
-            tsConfig: "./tsconfig.json",
-            "diagnostics": true
-        }
-    },
-    moduleFileExtensions: [
-        "ts",
-        "js"
-    ],
-    transform: {
-        "^.+\\.(ts|tsx)$": "ts-jest"
-    },
-    testMatch: [
-        "**/test/**/*.test.(ts|js)"
-    ],
-    testEnvironment: "node",
-    moduleNameMapper: {
-        "^src/(.*)$": "<rootDir>/src/$1"
-    },
+
+  globals: {
+    "ts-jest": {
+      tsConfig: "./tsconfig.json",
+      "diagnostics": true
+    }
+  },
+
+  moduleFileExtensions: [
+    "ts",
+    "js"
+  ],
+
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+
+  testMatch: [
+    "**/test/**/*.test.(ts|js)"
+  ],
+
+  testEnvironment: "node",
+
+  moduleNameMapper: {
+    "^src/(.*)$": "<rootDir>/src/$1"
+  },
+
 }

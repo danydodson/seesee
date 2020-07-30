8fb5cedc7634cc11c2cbfd04dc7ddc1d215ed3fe
Dany Dodson <danydodson@gmail.com>
2020-07-29 22:11:11 -0500

    changes

--- subscribers/rabbitmq/request.sh
+++
@@ -1,9 +0,0 @@
-# send a request to test the processing:
-curl --header "Content-Type: application/json" \
-  --request POST \
-  --data '{"data":"my-data"}' \
-  http://localhost:3000/api/v1/processData
-
-
-# Results saved
-# requestId: x , resultsData: my-data-processed

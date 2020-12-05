ed0dbb1556bfe05780129b63c36eb880449161c7
Dany Dodson <danydodsom@gmail.com>
2020-12-05 06:57:10 -0600

    a

--- gateway/lua/hello.lua
+++ gateway/lua/hello.lua
@@ -1,10 +1,11 @@
-local cjson = require "cjson"
+local cjson = require 'cjson'
 
 -- First define a json string
 local json_str ='{"name": "Bruce.Lin", "age": 25}'
+
 -- Turn it into an object here, and then output the attributes
 local json = cjson.decode(json_str)
-ngx.say("Name = ".. json['name'] .. ", Age =" .. tostring(json['age'])) -- here you need to convert 25 to a string before it can be a string Splicing
+ngx.say('Name = '.. json['name'] .. ', Age =' .. tostring(json['age'])) -- here you need to convert 25 to a string before it can be a string Splicing
 
 -- Output Name = Bruce.Lin, Age = 25
 
@@ -19,8 +20,8 @@ ngx.say(json_str2)
 ngx.say('<br/>') -- line break
 
 local obj = {
-ret = 200,
-msg = "login success"
+  ret = 200,
+  msg = 'login success'
 }
 
 ngx.say(cjson.encode(obj))
@@ -30,6 +31,6 @@ ngx.say('<br/>') -- line break
 local obj2 = {}
 
 obj2['ret'] = 200
-obj2['msg'] = "login fails"
+obj2['msg'] = 'login fails'
 
 ngx.say(cjson.encode(obj2))
--- gateway/lua/req.lua
+++ gateway/lua/req.lua
@@ -1,20 +1,28 @@
+--
+
 local _M = {}
 
--- Get http get/post request parameters
+-- Get get/post req params
 function _M.getArgs()
-    local request_method = ngx.var.request_method
-    local args = ngx.req.get_uri_args()
-    -- Parameter acquisition
-    if "POST" == request_method then
-        ngx.req.read_body()
-        local postArgs = ngx.req.get_post_args()
-        if postArgs then
-            for k, v in pairs(postArgs) do
-                args[k] = v
-            end
-        end
+
+  local args = ngx.req.get_uri_args()
+  local request_method = ngx.var.request_method
+
+  -- Param acquisition
+  if 'POST' == request_method then
+
+    ngx.req.read_body()
+    local postArgs = ngx.req.get_post_args()
+
+    if postArgs then
+      for k, v in pairs(postArgs) do
+        args[k] = v
+      end
     end
-    return args
+  end
+
+  return args
+
 end
 
 return _M

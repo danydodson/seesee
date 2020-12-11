a28e4febc9515bfc395b0d9d072fd2083db300f6
Dany Dodson <danydodsom@gmail.com>
2020-12-10 23:04:28 -0600

    a

--- proxy/lua/sign.lua
+++ proxy/lua/sign.lua
@@ -1,28 +1,23 @@
 
--- local req = require 'req'
 local cjson = require 'cjson'
-local jwt = require 'resty.jwt'
-
--- local args = req.getArgs()
-
--- local name = args['name']
+-- local jwt = require 'resty.jwt'
 
 local data = ngx.req.get_body_data()
 local json = cjson.decode(data)
 
-local jwt_token = jwt:sign(
-  'lua-resty-jwt',
-  {
-    header={typ='JWT', alg='HS256'},
-    payload = {
-      sub = '1234567890',
-      id = json['_id'],
-      name = json['name'],
-      role = json['role'],
-      iss = 'http://www.seesee.space',
-      exp = 604800, -- 1 week
-    }
-  }
-)
+-- local jwt_token = jwt:sign(
+--   'lua-resty-jwt',
+--   {
+--     header={typ='JWT', alg='HS256'},
+--     payload = {
+--       sub = '1234567890',
+--       id = json['_id'],
+--       name = json['name'],
+--       role = json['role'],
+--       iss = 'http://www.seesee.space',
+--       exp = 604800, -- 1 week
+--     }
+--   }
+-- )
 
-ngx.say(jwt_token)
+ngx.say(json)

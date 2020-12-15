de11949b187f4ec1c5d7628675c328932016d5e6
Dany Dodson <danydodsom@gmail.com>
2020-12-14 19:51:03 -0600

    a

--- status/index.js
+++ status/index.js
@@ -1,16 +1,16 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-var MARKER_FILE = "/var/marker.txt";
+var MARKER_FILE = "/var/marker.txt"
 
 app.get('/', function (req, res) {
-  res.sendfile(MARKER_FILE);
-});
+  res.sendFile(MARKER_FILE)
+})
 
 var server = app.listen(80, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Using markerfile: %s', MARKER_FILE);
-  console.log('every http get request succeeds if this file exists, fails otherwise');
-  console.log('http filecheck listening on http://%s:%s', host, port);
-});
+  var host = server.address().address
+  var port = server.address().port
+  console.log('Using markerfile: %s', MARKER_FILE)
+  console.log('every http get request succeeds if this file exists, fails otherwise')
+  console.log('http filecheck listening on http://%s:%s', host, port)
+})
